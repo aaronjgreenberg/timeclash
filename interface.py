8b7af659bf22c2ad646e4fe3858f3a6a25f1a388
Aaron Greenberg <aaronjgreenberg@gmail.com>
2012-03-06 20:51:23 +0800

    Added interface.py

---
+++ interface.py
@@ -0,0 +1,40 @@
+"""
+TimeClash
+=========
+An application to highlight the conflicts in your proposed semester schedule.
+
+This file contains the code that creates the user-interface used to access, query,
+and update the database that contains the schools and courses.
+
+CREATED
+-------
+By: Aaron Greenberg
+When: March 2012
+"""
+
+from Gui import *
+
+# Public: Methods and classes used to create a graphical user Interface to access,
+# update, and query a TimeClash database of courses and schools. All methods are
+# module methods and should be called on the Interface module.
+#
+# Examples
+#
+#   interface = Interface()
+#   # => <module 'Interface'>
+class Interface( Gui ):
+
+    # Public: Initialize an Interface.
+    def __init__( self ):
+        Gui.__init__( self )
+        self.title( 'TimeClash' )
+    
+    # Public: Run the event loop, wait for user to give input, return output.
+    #
+    # Returns nothing.
+    def run( self ):
+        self.mainloop()
+
+if __name__ == '__main__':
+    interface = Interface()
+    interface.run()
