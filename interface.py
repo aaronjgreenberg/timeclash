7c7e523e05e6510c485aa5b2ab28d4b61cbd0952
Aaron Greenberg <aaronjgreenberg@gmail.com>
2012-03-11 21:46:28 +0800

    Changed from menubuttons to listboxes, cleaned code

--- interface.py
+++ interface.py
@@ -6,6 +6,11 @@ An application to highlight the conflicts in your proposed semester schedule.
 This file contains the code that creates the user-interface used to access, query,
 and update the database that contains the schools and courses.
 
+TODO
+----
+* Make it so the user can dynamically add or remove the number of courses they
+  are taking.
+
 CREATED
 -------
 By: Aaron Greenberg
@@ -29,21 +34,22 @@ class Interface( Gui ):
         Gui.__init__( self )
         self.title( 'TimeClash' )
         self.resizable( 0, 0 )
+
         self.gr( cols = 2 )
         self.tc_button = self.bu( text = 'Time Check',
-                                  font = ( 'fixedsys', 14 ),
+                                  font = ( 'fixedsys', 12 ),
                                   width = 36,
                                   command = self.timecheck,
-                                  state = DISABLED
-                                  )
+                                  state = DISABLED )
 
         self.edit_button = self.bu( text = 'Edit Courses or Schools',
-                                   font = ( 'fixedsys', 14 ),
-                                   width = 36,
-                                   )
+                                    font = ( 'fixedsys', 12 ),
+                                    width = 37 )
+                                  
         self.endgr()
-        self.tc_bool = False
-        self.add_bool = False
+
+        self.tc_widglist = []
+        self.add_widglist = []
         self.timecheck()
 
     # Public: Create the widgets that allow a user to choose schools and courses
@@ -51,45 +57,37 @@ class Interface( Gui ):
     #
     # Returns nothing.
     def timecheck( self ):
+        self.tc_button.config( state = DISABLED )
         self.edit_button.config( state = NORMAL )
-        tc_widglist = []
-        tc_widglist.append( self.la( text = 'Please select your courses:',
-                                     font = ( 'fixedsys', 18, 'bold' ),
-                                     pady = 10 ) )
 
-        self.gr( cols = 2, background = 'black' )
-        tc_widglist.extend( [ 
-                self.mb( text = 'School',
-                         font = ( 'fixedsys', 14 ),
-                         background = 'white',
-                         width = 35,
-                         padx = 2,
-                         pady = 2,
-                         activebackground = '#737373' ),
-                self.mb( text = 'Course',
-                         font = ( 'fixedsys', 14 ),
-                         background = 'white',
-                         width = 35,
-                         padx = 2,
-                         pady = 2,
-                         activebackground = '#737373' ),
-                self.mb( text = 'School',
-                         font = ( 'fixedsys', 14 ),
-                         background = 'white',
-                         width = 35,
-                         padx = 2,
-                         pady = 2,
-                         activebackground = '#737373' ),
-                self.mb( text = 'Course',
-                         font = ( 'fixedsys', 14 ),
-                         background = 'white',
-                         width = 35,
-                         padx = 2,
-                         pady = 2,
-                         activebackground = '#737373' )
-                ] )
-                       
+        main_label = self.la( text = 'Please select your courses:',
+                              font = ( 'fixedsys', 18, 'bold' ),
+                              pady = 10 )
+
+
+        self.gr( cols = 2 )
+        
+        schoolbox = self.lb( font = ( 'fixedsys', 12 ),
+                             width = 37 )
+        coursebox = self.lb( font = ( 'fixedsys', 12 ),
+                             width = 37 )
+               
         self.endgr()
+
+        course_select = self.bu( text = 'Add course',
+                                 font = ( 'fixedsys', 14 ),
+                                 width = 74,
+                                 pady = 5 )
+
+        label_selected = self.la( text = 'Selected courses:',
+                                  font = ( 'fixedsys', 17 ),
+                                  pady = 8 )
+
+        selected_box = self.lb( font = ( 'fixedsys', 12 ),
+                                width = 74,
+                                height = 5,
+                                bg = '#DEDEDE' )
+                            
 
     # Public: Run the event loop, wait for user to give input, return output.
     #
